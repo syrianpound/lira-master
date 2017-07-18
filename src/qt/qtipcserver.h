08ac5ac1f781688312fbe4eb48a150b32697c5a1
syrianpound <syrianpoundchain@gmail.com>
2017-07-17 20:08:07 -0400

    lira
    first upload

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define lira-qt message queue name
+#define BITCOINURI_QUEUE_NAME "liraURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
